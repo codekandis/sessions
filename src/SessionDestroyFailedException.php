codekandis/session

Adds enhanced error handling.

src/SessionDestroyFailedException.php
@@ -1,0 +1,13 @@
+<?php declare( strict_types = 1 );
+namespace CodeKandis\Session;
+
+use LogicException;
+
+/**
+ * Represents an exception if a session has been failed to destroy.
+ * @package codekandis/session
+ * @author Christian Ramelow <info@codekandis.net>
+ */
+class SessionDestroyFailedException extends LogicException
+{
+}

src/SessionRegenerateIdFailedException.php
@@ -1,0 +1,13 @@
+<?php declare( strict_types = 1 );
+namespace CodeKandis\Session;
+
+use LogicException;
+
+/**
+ * Represents an exception if a session has been failed to regenerate its ID.
+ * @package codekandis/session
+ * @author Christian Ramelow <info@codekandis.net>
+ */
+class SessionRegenerateIdFailedException extends LogicException
+{
+}

src/SessionSetNameFailedException.php
@@ -1,0 +1,13 @@
+<?php declare( strict_types = 1 );
+namespace CodeKandis\Session;
+
+use LogicException;
+
+/**
+ * Represents an exception if a session has been failed to set its name.
+ * @package codekandis/session
+ * @author Christian Ramelow <info@codekandis.net>
+ */
+class SessionSetNameFailedException extends LogicException
+{
+}

src/SessionStartFailedException.php
@@ -1,0 +1,13 @@
+<?php declare( strict_types = 1 );
+namespace CodeKandis\Session;
+
+use LogicException;
+
+/**
+ * Represents an exception if a session has been failed to start.
+ * @package codekandis/session
+ * @author Christian Ramelow <info@codekandis.net>
+ */
+class SessionStartFailedException extends LogicException
+{
+}

src/SessionUnsetFailedException.php
@@ -1,0 +1,13 @@
+<?php declare( strict_types = 1 );
+namespace CodeKandis\Session;
+
+use LogicException;
+
+/**
+ * Represents an exception if a session has been failed to unset.
+ * @package codekandis/session
+ * @author Christian Ramelow <info@codekandis.net>
+ */
+class SessionUnsetFailedException extends LogicException
+{
+}

src/SessionWriteCloseFailedException.php
@@ -1,0 +1,13 @@
+<?php declare( strict_types = 1 );
+namespace CodeKandis\Session;
+
+use LogicException;
+
+/**
+ * Represents an exception if a session has been failed to write-close.
+ * @package codekandis/session
+ * @author Christian Ramelow <info@codekandis.net>
+ */
+class SessionWriteCloseFailedException extends LogicException
+{
+}

codekandis/sessions

src/SessionDestroyFailedException.php
@@ -1,0 +1,13 @@
+<?php declare( strict_types = 1 );
+namespace CodeKandis\Sessions;
+
+use LogicException;
+
+/**
+ * Represents an exception if a session has been failed to destroy.
+ * @package codekandis/sessions
+ * @author Christian Ramelow <info@codekandis.net>
+ */
+class SessionDestroyFailedException extends LogicException
+{
+}

src/SessionRegenerateIdFailedException.php
@@ -1,0 +1,13 @@
+<?php declare( strict_types = 1 );
+namespace CodeKandis\Sessions;
+
+use LogicException;
+
+/**
+ * Represents an exception if a session has been failed to regenerate its ID.
+ * @package codekandis/sessions
+ * @author Christian Ramelow <info@codekandis.net>
+ */
+class SessionRegenerateIdFailedException extends LogicException
+{
+}

src/SessionSetNameFailedException.php
@@ -1,0 +1,13 @@
+<?php declare( strict_types = 1 );
+namespace CodeKandis\Sessions;
+
+use LogicException;
+
+/**
+ * Represents an exception if a session has been failed to set its name.
+ * @package codekandis/sessions
+ * @author Christian Ramelow <info@codekandis.net>
+ */
+class SessionSetNameFailedException extends LogicException
+{
+}

src/SessionStartFailedException.php
@@ -1,0 +1,13 @@
+<?php declare( strict_types = 1 );
+namespace CodeKandis\Sessions;
+
+use LogicException;
+
+/**
+ * Represents an exception if a session has been failed to start.
+ * @package codekandis/sessions
+ * @author Christian Ramelow <info@codekandis.net>
+ */
+class SessionStartFailedException extends LogicException
+{
+}

src/SessionUnsetFailedException.php
@@ -1,0 +1,13 @@
+<?php declare( strict_types = 1 );
+namespace CodeKandis\Sessions;
+
+use LogicException;
+
+/**
+ * Represents an exception if a session has been failed to unset.
+ * @package codekandis/sessions
+ * @author Christian Ramelow <info@codekandis.net>
+ */
+class SessionUnsetFailedException extends LogicException
+{
+}

src/SessionWriteCloseFailedException.php
@@ -1,0 +1,13 @@
+<?php declare( strict_types = 1 );
+namespace CodeKandis\Sessions;
+
+use LogicException;
+
+/**
+ * Represents an exception if a session has been failed to write-close.
+ * @package codekandis/sessions
+ * @author Christian Ramelow <info@codekandis.net>
+ */
+class SessionWriteCloseFailedException extends LogicException
+{
+}