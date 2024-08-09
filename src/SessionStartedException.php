codekandis/session

Adds an exception on session start.

src/SessionStartedException.php
@@ -1,0 +1,13 @@
+<?php declare( strict_types = 1 );
+namespace CodeKandis\Session;
+
+use LogicException;
+
+/**
+ * Represents an exception if a session has been started.
+ * @package codekandis/session
+ * @author Christian Ramelow <info@codekandis.net>
+ */
+class SessionStartedException extends LogicException
+{
+}

codekandis/sessions

src/SessionStartedException.php
@@ -1,0 +1,13 @@
+<?php declare( strict_types = 1 );
+namespace CodeKandis\Sessions;
+
+use LogicException;
+
+/**
+ * Represents an exception if a session has been started.
+ * @package codekandis/sessions
+ * @author Christian Ramelow <info@codekandis.net>
+ */
+class SessionStartedException extends LogicException
+{
+}