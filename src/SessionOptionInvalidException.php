codekandis/session

Adds the necessary exception.

src/SessionOptionInvalidException.php
@@ -1,0 +1,13 @@
+<?php declare( strict_types = 1 );
+namespace CodeKandis\Session;
+
+use LogicException;
+
+/**
+ * Represents an exception if a session option is invalid.
+ * @package codekandis/session
+ * @author Christian Ramelow <info@codekandis.net>
+ */
+class SessionOptionInvalidException extends LogicException
+{
+}

codekandis/sessions

src/SessionOptionInvalidException.php
@@ -1,0 +1,13 @@
+<?php declare( strict_types = 1 );
+namespace CodeKandis\Sessions;
+
+use LogicException;
+
+/**
+ * Represents an exception if a session option is invalid.
+ * @package codekandis/sessions
+ * @author Christian Ramelow <info@codekandis.net>
+ */
+class SessionOptionInvalidException extends LogicException
+{
+}