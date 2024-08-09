codekandis/session

Adds the exception `SessionKeyNotFoundException`.

src/SessionKeyNotFoundException.php
@@ -1,0 +1,13 @@
+<?php declare( strict_types = 1 );
+namespace CodeKandis\Session;
+
+use LogicException;
+
+/**
+ * Represents an exception if a session key does not exist.
+ * @package codekandis/session
+ * @author Christian Ramelow <info@codekandis.net>
+ */
+class SessionKeyNotFoundException extends LogicException
+{
+}

codekandis/sessions

src/SessionKeyNotFoundException.php
@@ -1,0 +1,13 @@
+<?php declare( strict_types = 1 );
+namespace CodeKandis\Sessions;
+
+use LogicException;
+
+/**
+ * Represents an exception if a session key does not exist.
+ * @package codekandis/sessions
+ * @author Christian Ramelow <info@codekandis.net>
+ */
+class SessionKeyNotFoundException extends LogicException
+{
+}